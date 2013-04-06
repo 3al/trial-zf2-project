dad337813a918da9a41235b4ecb9620882ba89b1
Trishin Alexey <webmaster@trish.in>
2013-04-06 23:37:59 +0400

    dump DB

---
+++ trishin_zf2sa 20130406 2248.sql
@@ -0,0 +1,25 @@
+﻿-- Скрипт сгенерирован Devart dbForge Studio for MySQL, Версия 5.0.97.1
+-- Домашняя страница продукта: http://www.devart.com/ru/dbforge/mysql/studio
+-- Дата скрипта: 06.04.2013 22:48:47
+-- Версия сервера: 5.1.66-0ubuntu0.10.04.3
+-- Версия клиента: 4.1
+
+-- 
+-- Отключение внешних ключей
+-- 
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+
+-- 
+-- Установка кодировки, с использованием которой клиент будет посылать запросы на сервер
+--
+SET NAMES 'utf8';
+
+-- При генерации скрипта для объектов типа 'UDF'ы' произошла ошибка:
+-- SELECT command denied to user 'trishin_zf2sa'@'95-24-137-41.broadband.corbina.ru' for table 'func'
+
+-- Нет объектов для экспорта.
+
+-- 
+-- Включение внешних ключей
+-- 
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
